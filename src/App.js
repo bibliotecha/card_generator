1ab100c53db3f5632a9134890507e079f376f132
Kenichi Okiebisu <a01056715@gmail.com>
2021-01-13 21:50:52 -0800

    倍率カウンターkannsei

--- src/App.js
+++ src/App.js
@@ -1,25 +1,76 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class Counter extends React.Component {
+  state = {
+    suuji: 1,
+    bairitsu: 1,
+    result: 1,
+  };
+
+  render() {
+    return (
+      <div
+        style={{
+          height: "100vh",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <div>
+          <h2>数字</h2>
+          <h1>{this.state.suuji}</h1>
+          <div>
+            <button
+              onClick={() => this.setState({ suuji: this.state.suuji + 1 })}
+            >
+              +
+            </button>
+            <button
+              onClick={() => this.setState({ suuji: this.state.suuji - 1 })}
+            >
+              -
+            </button>
+          </div>
+        </div>
+        <div>
+          <h2>倍率</h2>
+          <h1>{this.state.bairitsu}</h1>
+          <div>
+            <button
+              onClick={() =>
+                this.setState({ bairitsu: this.state.bairitsu + 1 })
+              }
+            >
+              +
+            </button>
+            <button
+              onClick={() =>
+                this.setState({ bairitsu: this.state.bairitsu - 1 })
+              }
+            >
+              -
+            </button>
+          </div>
+        </div>
+        <div>
+          <h2>結果</h2>
+          <h1>{this.state.result}</h1>
+          <div>
+            <button
+              onClick={() =>
+                this.setState({
+                  result: this.state.suuji * this.state.bairitsu,
+                })
+              }
+            >
+              かける
+            </button>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
-export default App;
+export default Counter;
