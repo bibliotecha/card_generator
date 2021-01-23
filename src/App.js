36fa57e726caa2ab985ddf140678c441a2d03820
ryotaka35 <ha34ta@gmail.com>
2021-01-23 17:41:20 +0900

    カード課題

--- src/App.js
+++ src/App.js
@@ -7,26 +7,30 @@ class Dashboard extends React.Component {
       <div>
         <div>dashboard</div>
         <div style={{ display: "flex" }}>
-          <Card
+          <div className="dog">
+           <Card
             title="子犬"
             description="子犬の説明"
-            imgUrl="https://images.pexels.com/photos/1108099/pexels-photo-1108099.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
-          />
+            imgUrl="https://images.pexels.com/photos/1108099/pexels-photo-1108099.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"/>
+          </div>
           {/* <Card
             title="不明"
             description="description"
             imgUrl="https://www.cdc.gov/healthypets/images/pets/cute-dog-headshot.jpg"
           /> */}
+          <div className="dog">
           <Card
             title="レトリーバー"
             description="レトリーバーの説明"
             imgUrl="https://thecatandthedog.com/wp-content/uploads/2020/11/105992231-1561667465295gettyimages-521697453.jpeg"
           />
+          </div>
+          <div className="dog">
           <Card
             title="パグ"
             description="パグの説明"
             imgUrl="https://post.healthline.com/wp-content/uploads/2020/08/3180-Pug_green_grass-732x549-thumbnail-732x549.jpg"
-          />
+          /></div>
         </div>
       </div>
     );
